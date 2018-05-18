49835feb7d3f1356049ae7fc53bc38c25e3a62a5
Bora Yuksel <borayuksel1903@gmail.com>
2018-05-18 14:09:41 -0700

    Added README and comments for Problem 2

--- AddTwoNumbers/List.java
+++ AddTwoNumbers/List.java
@@ -1,58 +1,100 @@
+/**
+ * Class description: creates a new Circular List, where Nodes are connected to
+ * each other through pointers. 
+ */
 public class List{
-	public Node front;
-	public int occupancy;
+	public Node front; //pointer that points to the first node
+	public int occupancy; //occupancy of the list
 
+	/**
+	 * Description: constructor that creates a new empty list
+	 */
 	public List(){
-		front = null;
-		occupancy = 0;
+		front = null; //sets front pointer to null
+		occupancy = 0; //initializes occupancy to 0
 	}
-
+   
+	/**
+	 * Description: takes in an integer to store inside the Node, creates a new
+	 * 				 Node with the data, inserts the Node. 
+	 *
+	 * @param: int data- data to be stored inside the lis by a Node
+	 */
 	public void insert(int data){
-		Node working = new Node(data);
+		Node working = new Node(data); //creates a new Node with the data stored 
 
-		Node.insertNode(this, working);
-		occupancy++;
+		working.insertNode(this); //inserts the node to the list
+		occupancy++; //increments occupancy
 	}
-
+   
+	/**
+	 * Description: creates a new number with the elements of the List in a
+	 * 				 reverse order. i.e. 3->4->5 becomes the integer 543
+	 *
+	 * @return: int- the number that was resulted from the list
+	 */
 	public int reverseOrder(){
-		Node working = this.front.pre;
-		int result = working.getData();
-		working = working.pre;
-
+		Node working = this.front.pre; //starts from the last Node in the List
+		int result = working.getData(); //gets the data stored in the last Node
+		working = working.pre; //moves back a Node
+      
+		//loops through each Node in the List
 		while(working != this.front.pre){
+			//creates a new number throught the data from the Nodes
 			result = (result * 10) + working.getData();
 			working = working.pre;
 		}
 
-		return result;
+		return result; //returns the number created
 	}
 
+	/**
+	 * Description: inserts each number from the input integer into the List in a
+	 * 				 reverse order
+	 *
+	 * @param: int sum- integer to convert to a List
+	 */
 	public void intToList(int sum){
+		//loops through each number in input
 		while(sum != 0){
-			int remainder = sum % 10;
-			sum /= 10;
+			int remainder = sum % 10; //gets the last element from the input
+			sum /= 10; //deletes the last number from the input
 
-			this.insert(remainder);
+			this.insert(remainder); //inserts the last element to the List
 		}
 	}
 
+	/**
+	 * Description: takes in two circular linked Lists and adds the data stored
+	 * 				 inside their Nodes in the reverse order. Then, creates a new
+	 * 				 List with the new sum, each integer stored in a reverse order
+	 *
+	 * @param: List list1- first List to get the number from
+	 * @param: List list2- second List to get the number from
+	 * @return: List- List containing the sum stored in reverse order
+	 */
 	public static List addTwoNumbers(List list1, List list2){
-		List result = new List(); 
+		List result = new List(); //creates a new List to store the sum
 		
+		//calculates the sum from the input Lists
 		int sum = list1.reverseOrder() + list2.reverseOrder();
 
-		result.intToList(sum);
+		result.intToList(sum); //converts the sum into a new List
 		
-		return result;	
+		return result;	//returns the List
 	}
-
+   
+	/**
+	 * Description: writes the List on the terminal
+	 */
 	public void writeList(){
-		Node working = this.front; 
+		Node working = this.front; //stores the front Node from List
 		
+		//loops through each Node and prints data from each Node
 		while(working != this.front.pre){
 			System.out.print(working.getData() + " -> ");
 			working = working.next;
 		}
-		System.out.println(working.getData());
+		System.out.println(working.getData()); //prints data from last Node
 	}
 }
--- AddTwoNumbers/Node.java
+++ AddTwoNumbers/Node.java
@@ -1,36 +1,67 @@
 public class Node{
-	private int data; 
-	public Node next;
-	public Node pre;
-
+	private int data; //to store the digit inside the node 
+	public Node next; //pointer to the previous node
+	public Node pre; //pointer to the next node
+   
+	/**
+	 * Description: constructor that takes in an integer to store inside the
+	 * 				 node. Pre and next pointers point to null.
+	 *
+	 * @param: int data- data to store inside the node
+	 */
 	public Node(int data){
 		this.data = data; //stores the data inside the Node
-		next = this; //sets nextNode to null
-		pre = this; //sets preNode to null
+		next = null; //sets nextNode to null
+		pre = null; //sets preNode to null
 	}
 
-
+   
+	/**
+	 * Description: getter method that gets the private data field from the
+	 * 				 calling object
+	 *
+	 * @return: int- the integer stored in the data field
+	 */
 	public int getData(){
-		return this.data;
+		return this.data; //returns the data field
 	}
 
+	/**
+	 * Description: setter method that sets the data field of the calling
+	 * 				 object
+	 *
+	 * @param: int data- the new data to be stored inside node
+	 */
 	public void setData(int data){
-		this.data = data;
+		this.data = data; //sets data field to the input int
 	}
 
-	public static void insertNode(List list, Node node){
-		if(list.front == null)
-			list.front = node;
+	/**
+	 * Description: inserts the node to the list by attaching and integrating the
+	 * 				 node the list at the end of the list
+	 *
+	 * @param: List list- list to insert the new node in to
+	 */
+	public void insertNode(List list){
+		//checks if list is empty
+		if(list.front == null){
+			//front points to the new node
+			list.front = this;
+
+			//pre and next pointers point to the node to be inserted
+			this.pre = this.next = this;
+		}
+		//otherwise, attach and integrate with the other nodes
 		else{
 			Node working = list.front;
 			
 			//attach the node
-			node.pre = list.front.pre;
-			node.next = list.front;
+			this.pre = list.front.pre;
+			this.next = list.front;
 
 			//integrate the node
-			node.pre.next = node;
-			list.front.pre = node;
+			this.pre.next = this;
+			list.front.pre = this;
 		}			
 	}
-}
+} //end of Node class
